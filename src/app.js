716e63298092b7b6d8a6af7eeb8784bfe74c1208
Ann Paiuk <annvseslavskaya@gmail.com>
2023-09-21 21:34:58 +0300

    form submition

--- src/app.js
+++ src/app.js
@@ -2,3 +2,24 @@
 
 // Styles
 import "/assets/scss/_main.scss";
+
+let button = document.getElementById("form-button");
+let inputName = document.getElementById("input-name");
+let inputEmail = document.getElementById("input-email");
+let inputMessage = document.getElementById("input-message");
+
+button.onclick = (e) => {
+    fetch("https://api.byteplex.info/api/test/contact/", {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name: inputName.value,
+            email: inputEmail.value,
+            message: inputMessage.value
+        }),
+    });
+
+    e.preventDefault();
+}
